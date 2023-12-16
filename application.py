ef93255a8fc0cc8c9050258b65c246dae926b559
Abdullah Javed <majaved770@gmail.com>
2023-12-16 18:31:19 +0500

    Updated both HTML and Python files

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 #importing flaskpython -m flask run
-from flask import Flask
+from flask import Flask, render_template,session
 from flask_session import Session
 from tempfile import mkdtemp
 
@@ -9,11 +9,12 @@ app = Flask(__name__)
 app.config["SESSION_FILE_DIR"]=mkdtemp()
 app.config["SESSION_PERMANENT"] =False
 app.config["SESSION_TYPE"] ="filesystem"
+Session(app)
+
 app.route("/")
 def index():
-    if "board" not in Session:
-        Session["board"]=[[None,None,None],[None,None,None],[None,None,None]]
-        Session["turn"]="X"
-        
-        return render_template("game.html", game= session["board"], turn= session["turn"])
+    if "board" not in session:
+        session["board"]=[[None,None,None],[None,None,None],[None,None,None]]
+        session["turn"]="X"
 
+        return render_template("game.html", game= session["board"], turn= session["turn"])
