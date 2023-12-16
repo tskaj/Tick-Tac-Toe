27e58fa79afc4e9ba261517e918c06b6a34e1137
Abdullah Javed <majaved770@gmail.com>
2023-12-16 18:13:35 +0500

    Pushing the configured application.py and an empty game.html

---
+++ application.py
@@ -0,0 +1,18 @@
+#importing flaskpython -m flask run
+from flask import Flask
+from flask_session import Session
+from tempfile import mkdtemp
+
+app = Flask(__name__)
+
+#creating twmporary directory to store the session data 
+app.config["SESSION_FILE_DIR"]=mkdtemp()
+app.config["SESSION_PERMANENT"] =False
+app.config["SESSION_TYPE"] ="filesystem"
+app.route("/")
+def index():
+    if "board" not in Session:
+        Session["board"]=[[None,None,None],[None,None,None],[None,None,None]]
+        Session["turn"]="X"
+        
+
