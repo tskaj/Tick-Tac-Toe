e5c692056ab12afcbb237983480a077157f3e387
Abdullah Javed <majaved770@gmail.com>
2023-12-16 19:24:45 +0500

    Final Commit

--- application.py
+++ application.py
@@ -1,20 +1,36 @@
-#importing flaskpython -m flask run
-from flask import Flask, render_template,session
+from flask import Flask, render_template, session, redirect, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 
 app = Flask(__name__)
 
-#creating twmporary directory to store the session data 
-app.config["SESSION_FILE_DIR"]=mkdtemp()
-app.config["SESSION_PERMANENT"] =False
-app.config["SESSION_TYPE"] ="filesystem"
+# Creating temporary directory to store the session data
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app.route("/")
+@app.route("/")
 def index():
     if "board" not in session:
-        session["board"]=[[None,None,None],[None,None,None],[None,None,None]]
-        session["turn"]="X"
+        session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+        session["turn"] = "X"
+    return render_template("game.html", game=session["board"], turn=session["turn"])
 
-        return render_template("game.html", game= session["board"], turn= session["turn"])
+@app.route("/play/<int:row>/<int:col>")
+def play(row, col):
+    # UpdateD the session data or game state based on the row and column selected
+    
+    session["board"][row][col] = session["turn"]
+    # Added logic to handle the game moves
+
+    # Redirect back to the game page after the move
+    return redirect(url_for("index"))
+
+@app.route("/reset")
+def reset():
+    # ResetS the game state or session data
+    session.clear()
+    return redirect(url_for("index"))
+
+#END OF THE GAME
